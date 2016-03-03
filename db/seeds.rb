22b03714a5803eb6e05fd76eb5b13375b280bb4e
Skylar King <sking@crazyawesome.io>
2016-03-03 01:37:24 -0600

    Add more styling.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ if Author.all.blank?
 end
 
 if Book.all.blank?
-  Book.create title: "Test book", photo_url: "www.google.com", price: 10.00, author_id: "1"
+  Book.create title: "Harry Potter", photo_url: "http://cdn.pastemagazine.com/www/articles/harry%20potter%20world%20.jpg", price: 50.00, author_id: "1"
 end
