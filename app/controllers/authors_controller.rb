e26ca885b626c05a2f9e1e7bb15d1d1c22810070
Skylar King <sking@crazyawesome.io>
2016-03-02 23:07:00 -0600

    Update controller. Add relationships. Add temp nav.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,22 +1,39 @@
 class AuthorsController < ApplicationController
   def index
+    @authors = Author.all
   end
 
   def new
+    @author = Author.new
   end
 
   def create
+    @author = Author.new
+    @author.first_name = params[:author][:first_name]
+    @author.last_name = params[:author][:last_name]
+    @author.bio = params[:author][:bio]
+    @author.save ? (redirect_to authors_path) : (render :new)
   end
 
   def show
+    @author = Author.find_by id: params[:id]
   end
 
   def edit
+    @author = Author.find_by id: params[:id]
   end
 
   def update
+    @author = Author.find_by id: params[:id]
+    @author.first_name = params[:author][:first_name]
+    @author.last_name = params[:author][:last_name]
+    @author.bio = params[:author][:bio]
+    @author.save ? (redirect_to author_path) : (render :edit)
   end
 
   def delete
+    @author = Author.find_by id: params[:id]
+    @author.destroy
+    redirect_to authors_path
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,39 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new
+    @book.title = params[:book][:title]
+    @book.photo_url = params[:book][:title]
+    @book.price = params[:book][:price]
+    @book.save ? (redirect_to books_path) : (render :new)
   end
 
   def show
+    @book = Book.find_by id: params[:id]
   end
 
   def edit
+    @book = Book.find_by id: params[:id]
   end
 
   def update
+    @book = Book.find_by id: params[:id]
+    @book.title = params[:book][:title]
+    @book.photo_url = params[:book][:photo_url]
+    @book.price = params[:book][:price]
+    @book.save ? (redirect_to book_path) : (render :edit)
   end
 
   def delete
+    @book = Book.find_by id: params[:id]
+    @book.destroy
+    redirect_to books_path
   end
 end
