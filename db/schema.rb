c9af15c65cdd441386b36fde5d4e5bbbadeb759d
Skylar King <sking@crazyawesome.io>
2016-03-03 10:04:27 -0600

    Added Refile, RefilePsql gems.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303050822) do
+ActiveRecord::Schema.define(version: 20160303160109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,5 +32,11 @@ ActiveRecord::Schema.define(version: 20160303050822) do
     t.datetime "updated_at", null: false
     t.integer  "author_id"
   end
+
+  create_table "refile_attachments", force: :cascade do |t|
+    t.string "namespace", null: false
+  end
+
+  add_index "refile_attachments", ["namespace"], name: "index_refile_attachments_on_namespace", using: :btree
 
 end
