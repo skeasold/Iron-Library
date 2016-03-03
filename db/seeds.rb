38e0a4f377e109a992b8e668adf6d72363ad91de
Skylar King <sking@crazyawesome.io>
2016-03-03 03:50:10 -0600

    Update styling author & book. Update new seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 if Author.all.blank?
-  Author.create first_name: "Test", last_name: "Author", bio: "Bio here"
+  Author.create id: "1", first_name: "Harvey", last_name: "Ipsum", bio: "Climb leg rub face on everything give attitude nap all day for under the bed. Chase mice attack feet but..."
 end
 
 if Book.all.blank?
-  Book.create title: "Harry Potter", photo_url: "http://cdn.pastemagazine.com/www/articles/harry%20potter%20world%20.jpg", price: 50.00, author_id: "1"
+  Book.create title: "Jedi-Foo Rat!", photo_url: "http://images6.fanpop.com/image/photos/35200000/funny-animals-35204748-500-331.jpg", price: 10.5, author_id: "1"
 end
