246d4726e1af4040772932fc944365e1820cab6e
Skylar King <sking@crazyawesome.io>
2016-03-02 22:28:40 -0600

    Add MVC.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'authors/index'
+
+  get 'authors/new'
+
+  get 'authors/show'
+
+  get 'authors/edit'
+
+  get 'books/index'
+
+  get 'books/new'
+
+  get 'books/show'
+
+  get 'books/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
