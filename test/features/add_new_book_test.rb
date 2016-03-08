532881c85ff0a107ad51f7eb00897bb80327e4d7
Skylar King <sking@crazyawesome.io>
2016-03-08 12:46:41 -0600

    Complete new book minitest. Passed.

--- test/features/add_new_book_test.rb
+++ test/features/add_new_book_test.rb
@@ -1,9 +1,21 @@
 require "test_helper"
 
 class AddNewBookTest < Capybara::Rails::TestCase
-  test "sanity" do
-    visit root_path
-    assert_content page, "Hello World"
-    refute_content page, "Goobye All!"
+  setup do
+    Author.create first_name: "First", last_name: "Last", bio: "Bio text"
+  end
+
+  test "Add book" do
+  visit new_book_path
+    within ("#new_book") do
+      fill_in "Title", with: "Test Book"
+      fill_in "Photo url", with: "www.google.com"
+      fill_in "Price", with: "10.00"
+      fill_in "Author", with: "1"
+      click_button "Create Book"
+    end
+  visit root_path
+  save_and_open_page
+  assert_content page, "Test Book"
   end
 end
