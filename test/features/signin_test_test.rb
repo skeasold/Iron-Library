6d117dd209ca757cffeceb0e9e3dad8a0f8a713b
Skylar King <sking@crazyawesome.io>
2016-03-10 04:38:46 -0600

    Add login test. Update all other tests.

--- test/features/signin_test_test.rb
+++ test/features/signin_test_test.rb
@@ -1,27 +1,19 @@
 require "test_helper"
 
-class AddNewBookTest < Capybara::Rails::TestCase
+class SigninTestTest < Capybara::Rails::TestCase
   setup do
     User.create! first_name: "First", last_name: "Last", email: "test@test.com", password: "test12345"
     Author.create! id: "1", first_name: "First", last_name: "Last", bio: "Bio text"
+    Book.create! title: "Test Book", photo_url: "www.google.com", price: ".99", author_id: "1"
   end
-
-  test "Add book" do
+  test "sign in and view book" do
     visit root_path
     click_link "Login"
     fill_in "Email", with: "test@test.com"
     fill_in "Password", with: "test12345"
     click_button "Login"
-
-    visit new_book_path
-      within ("#new_book") do
-        fill_in "Title", with: "Test Book"
-        fill_in "Photo url", with: "www.google.com"
-        fill_in "Price", with: "10.00"
-        fill_in "Author", with: "1"
-        click_button "Create Book"
-      end
     visit root_path
+    click_link "More Details"
     assert_content page, "Test Book"
   end
 end
